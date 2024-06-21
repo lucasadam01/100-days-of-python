89d9c76a1c48211a3d3ebb1738dbeeac66091353
lucasadam01 <lucaslima0881@gmail.com>
2024-06-20 21:26:15 -0300

    Atualização no projeto do dia X do desafio Python de 100 dias

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Gerador de Nome de Banda!\n")
+
+cidade = input("Qual é a cidade em que você nasceu?\n")
+pet = input("\nQual é o nome do seu pet?\n")
+
+print("\nO nome da sua banda poderia se chamar " + cidade + " " + pet)
